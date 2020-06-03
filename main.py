08b8abcceb80f35b770bc623ea5fa6e0c5d0b2bf
Carolyn <ckaminski@albany.edu>
2020-06-03 00:25:27 -0400

    Add two way doors

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ import colors
 from rooms import Room, Person, Map
 
 
-def make_rooms():
-    hole = Room("The Hole", "You have fallen down a hole into a dungeon.")
-    cave = Room("The Cave", "A dark cave. You can't see anything.")
+def create_dungeon():
+    dungeon = Map()
+    dungeon.add_room("The Hole", "You have fallen down a hole into a dungeon.")
+    dungeon.add_room("The Cave", "A dark cave. You can't see anything.")
     falls = Room("Underground falls.", "A waterfall rushes from the ceiling.")
-    farm = Room("The Farm", "Stairs lead to a light doorway. You the realize it is aboveground! Then cows, sheep, and some chicken signat it is a farm.")
-    bunker = Room("The Bunker", "A long room with dozens of bunkbeds line the walls. They look long abandoned. Cobwebs are everywhere.")
-    control = Room("The Control center", "There are three giant panels here with blinking lights and hundreds of knobs and buttons. There is what looks like a pilot seat facing a wide screen.")
+    farm = Room("The Farm",
+                "Stairs lead to a light doorway. You the realize it is aboveground! Then cows, sheep, and some chicken signat it is a farm.")
+    bunker = Room("The Bunker",
+                  "A long room with dozens of bunkbeds line the walls. They look long abandoned. Cobwebs are everywhere.")
+    control = Room("The Control center",
+                   "There are three giant panels here with blinking lights and hundreds of knobs and buttons. There is what looks like a pilot seat facing a wide screen.")
 
-    hole.doors['e'] = cave
-    cave.doors['n'] = falls
-    falls.doors['s'] = farm
-    farm.doors['n'] = bunker
-    bunker.doors['w'] = control
+    dungeon.add_door("The Hole", "The Cave", "e")
+    # hole.doors['e'] = cave
+    # cave.doors['n'] = falls
+    # falls.doors['s'] = farm
+    # farm.doors['n'] = bunker
+    # bunker.doors['w'] = control
 
-
-    return map
-
+    return dungeon
 
 
 def find_doors(room):
@@ -29,20 +32,21 @@ def find_doors(room):
     room: Room
       where the player currently is
     """
+    escape = True
     if 'e' in room.doors:
-      dir = "east"
+        direction = "east"
     elif 'w' in room.doors:
-      dir = "west"
+        direction = "west"
     elif 'n' in room.doors:
-      dir = "north"
+        direction = "north"
     elif 's' in room.doors:
-      dir = "south"
+        direction = "south"
+    else:
+        escape = False
+    if escape:
+        print(colors.orange + "There is a door to the " + direction)
     else:
         print("There are no doors")
-        dir = "nowhere"
-    print(colors.orange + "There is a door to the: " + dir)
-
-
 
 def get_menu():
     return """
@@ -56,25 +60,25 @@ ask - ask the person a question
 
 def play():
     ans = ''
-    # inventory = []
-    map = Map()
-    current_room = map.rooms[1]
+    dungeon = create_dungeon()
+    current_room = dungeon.rooms["The Hole"]
     print(colors.green + current_room.name)
     print(colors.pink + current_room.description)
     if current_room.people:
-      print(str(current_room.people[0].name) + " is here.")
+        print(str(current_room.people[0].name) + " is here.")
     while ans != 'q':
         find_doors(current_room)
         print(colors.default)
         ans = input("""\nWhat do you want to do? (type help to see options.) """)
+        reply = " "
         if ans == "help":
             reply = get_menu()
         elif ans == "e" or ans == "w" or ans == "n" or ans == "s":
-            if current_room.walk(ans) is not None:
-                current_room = current_room.walk(ans)
+            if dungeon.walk(current_room, ans) is not None:
+                current_room = dungeon.walk(current_room, ans)
             reply = colors.green + current_room.name + "\n" + colors.pink + current_room.description
         elif ans == "ask" and current_room.people:
-          current_room.people[0].ask()
+            current_room.people[0].ask()
         elif ans == "q":
             exit()
         else:
@@ -82,12 +86,7 @@ def play():
 
         print(reply)
         if current_room.people:
-          print(str(current_room.people[0].name) + " is here.")
-        print(" ")
+            print(str(current_room.people[0].name) + " is here.")
+
 
 play()
-
-
-
-
-
--- rooms.py
+++ rooms.py
@@ -1,4 +1,5 @@
-import colors  
+import colors
+
 
 class Room:
     """
@@ -16,47 +17,58 @@ class Room:
     def __init__(self, name, desc):
         """
         Make a room
+        doors = name:direction
         """
         self.name = name
         self.description = desc
-        self.doors = {}
         self.items = []
+        self.doors = {}
         self.people = []
-    
-    def add_neighbor(self, dir):
-        self.doors.add(dir)
 
-    def walk(self, direction):
-        if direction in self.doors:
-            new_room = self.doors[direction]
-            return new_room
-        else:
-            print("You can't go that way.")
+    def add_neighbor(self, neighbor_name, direction):
+        self.doors[direction] = neighbor_name
+
 
 class Map:
-    def __init__(self, rooms={}):
+    def __init__(self, rooms=None):
         """
         Make a map
+
         """
+        if not rooms:
+            rooms = {}
         self.rooms = rooms
 
-    def add_room(self, name):
-        new_room = Room(name)
+    def add_room(self, name, desc=""):
+        new_room = Room(name, desc)
         self.rooms[name] = new_room
 
     def get_room(self, name):
         if name in self.rooms.keys():
-            return self.nodelist[name]
+            return self.rooms[name]
         return None
 
-    def add_oneway_door(self, fr, to):
+    def add_trap_door(self, fr, to, direction):
+        """
+        A directed edge between rooms
+        """
         if self.get_room(fr) is None:
             self.add_room(fr)
         if self.get_room(to) is None:
             self.add_room(to)
-        self.get_room(fr).add_neighbor(to)
-  
+        self.get_room(fr).add_neighbor(to, direction)
 
+    def add_door(self, id1, id2, direction):
+        mirror = {"e": "w", "w": "e", "n": "s", "s": "n"}
+        self.add_trap_door(id1, id2, direction)
+        self.add_trap_door(id2, id1, mirror[direction])
+
+    def walk(self, fr, direction):
+        if fr.doors[direction]:
+            new_room = self.get_room(fr.doors[direction])
+            return new_room
+        else:
+            print("You can't go that way.")
 
 
 class Person:
@@ -78,11 +90,10 @@ class Person:
         self.questions = []
         self.answers = []
 
-
     def ask(self):
-      for q in self.questions:
-          print(q)
-      ans = input("What do you want to ask? ")
-      print("\n" + colors.pink + self.name + " replies: ")
-      print(self.answers[int(ans)-1])
-      print("\n")
+        for q in self.questions:
+            print(q)
+        ans = input("What do you want to ask? ")
+        print("\n" + colors.pink + self.name + " replies: ")
+        print(self.answers[int(ans) - 1])
+        print("\n")
